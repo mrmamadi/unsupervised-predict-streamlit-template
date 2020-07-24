71bedfe2a466efbcbb6b41b908b6a146faeb055d
Kgaogelo Mamadi <mrmamadi@outlook.com>
2020-07-24 15:08:04 +0000

    Added group content recommender

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview", "EDA", "Contributors"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -90,12 +90,60 @@ def main():
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
+        titles_list = sorted(title_list)
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
+        st.image('resources/imgs/Image_header.png',use_column_width=True)
+        # Recommender System algorithm selection
+        sys = st.radio("Select an algorithm",
+                       ('Content Based Filtering',
+                        'Collaborative Based Filtering'))
 
+        # User-based preferences
+        st.write('### Enter Your Three Favorite Movies')
+        movie_1 = st.selectbox('Fisrt Option',(titles_list[1100:1200]))
+        movie_2 = st.selectbox('Second Option',(titles_list[2366:2466]))
+        movie_3 = st.selectbox('Third Option',(titles_list[3666:3766]))
+        # movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        # movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        # movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        fav_movies = [movie_1,movie_2,movie_3]
+
+        # Perform top-10 movie recommendation generation
+        if sys == 'Content Based Filtering':
+            top_recommendations = content_model(fav_movies)
+            if st.button("Recommend"):
+                   st.title("We think you'll like:")
+                   for i,j in enumerate(top_recommendations[:10]):
+                       st.subheader(str(i+1)+'. '+j)
+
+        if sys == 'Collaborative Based Filtering':
+            top_recommendations = collab_model(movie_list=fav_movies,
+                                               top_n=10)
+            if st.button("Recommend"):
+                st.title("We think you'll like:")
+                for i,j in enumerate(top_recommendations):
+                    st.subheader(str(i+1)+'. '+j)
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
+    # ------------------------------------------------------------------------
 
+
+    # ------------------------------- NEW PAGE -------------------------------
+    if page_selection == "EDA":
+
+        st.title("Exploratory Data Analysis")
+
+
+
+    # ------------------------------------------------------------------------
+
+
+    # ------------------------------- NEW PAGE -------------------------------
+    if page_selection == "Contributors":
+
+        st.title("Team EN1 JHB Unsupervised")
+    
 if __name__ == '__main__':
     main()
--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -5,54 +5,43 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
-os.path.exists('../resources/data/movies.csv')
+# os.path.exists('../resources/data/movies.csv')
 
 # Importing data
+movies = pd.read_csv('../movies.csv') # Remove this to be modular
+# ratings = pd.read_csv('resources/data/ratings.csv') # Remove this to be modular
 
-movies = pd.read_csv('resources/data/movies.csv', sep = ',',delimiter=',') # Remove this to be modular
-ratings = pd.read_csv('resources/data/ratings.csv') # Remove this to be modular
-movies.dropna(inplace=True)
+# Initialising Vectoriser
+vectoriser = CountVectorizer()
+# Fitting Vectoriser and transforming content column
+content_matrix = vectoriser.fit_transform(movies['content'])
 
-def datapreprocessing(subset_size):
-    # spliting the genres
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
-    # Subset of the data
-    movies_subset = movies[:subset_size]
-    return movies_subset
+# movieIds = movies['movieId']
 
-def content_model(list_title):
-    # initializing the empty list of recommended movies
-    recommended_movies = []
-    data = datapreprocessing(10000)
-    # instantiating and generating the count matrix
-    count_vec = CountVectorizer()
-    count_matrix = count_vec.fit_transform(data['keyWords'])
-    indices = pd.Series(data['title'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    # gettin the index of the movie that matches the title
-    idx_1 = indices[indices == list_title[0]].index[0]
-    idx_2 = indices[indices == list_title[1]].index[0]
-    idx_3 = indices[indices == list_title[2]].index[0]
-    # creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+# Creating Series of movieIds and indices for easier recall
+indices = pd.Series(movies.index, index=movies['movieId'])
 
-    #listings.drop_duplicates(inplace=True)
+def content_model(list_title,k=20):
 
-    # a list to store names
-    recommended_movies = []
-    #movie_list
-    # appending the names of movies
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:21]:
-        recommended_movies.append(list(movies['title'])[i])
-    return recommended_movies
+    # Vectorise content for each movie in list_title
+    input_matrix = vectoriser.transform(movies.content[movies['title'].isin(list_title)])
+
+    # Initiate list to store indeces of input movies
+    m_idx = []
+    for title in list_title:
+        m_idx.append(indices[movies.movieId[movies['title']==title]].values)
+    # Create list of similarities between each input movie and every other movie in the dataset                   
+    sim = list(enumerate(cosine_similarity(content_matrix,
+                                       input_matrix)))   
+
+    # Sort the list by the average similarity of the movies
+    sim_scores = sorted(sim, key=lambda x: x[1].mean(), reverse=True)
+                       
+    # Select the top-k values for recommendation
+    sim_scores = sim_scores[0:k]
+
+    # Select the indices of the top-k movies
+    movie_indices = [i[0] for i in sim_scores if i[0] not in m_idx]
+    
+    # Return a list of the movie titles
+    return movies.iloc[movie_indices].title   
